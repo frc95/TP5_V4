cf8c7943deff52adcb43f9fc9da44d57c7ba7e9c
frc95 <frc95zz@gmail.com>
2020-05-24 22:54:49 -0300

    first commit

---
+++ backend/mostrarPDF.php
@@ -0,0 +1,58 @@
+<?php
+require_once __DIR__ . '/vendor/autoload.php';
+require "persona.php";
+require "empleado.php";
+require "fabrica.php";
+
+
+
+header('content-type:application/pdf');
+
+session_start();
+
+$mpdf = new \Mpdf\Mpdf(['orientation' => 'P', 
+                        'pagenumPrefix' => 'Página nro. ',
+                        'pagenumSuffix' => ' - ',
+                        'nbpgPrefix' => ' de ',
+                        'nbpgSuffix' => ' páginas']);
+
+$mpdf->SetProtection(array(), $_SESSION["DNIEmpleado"], $_SESSION["DNIEmpleado"]);
+
+$mpdf->SetHeader('Amarilla Godoy Franco Nahuel||{PAGENO}{nbpg}');
+$mpdf->setFooter('https://tp5-pdo-franco-v4.herokuapp.com/||{PAGENO}');
+
+$datos="";
+$fabrica= new Fabrica("Grupo UTN");
+$fabrica->TraerDeArchivo("archivos/empleados.txt");
+$empleados=$fabrica->GetEmpleados();
+foreach($empleados as $empleado)
+{
+    $path=$empleado->GetPathFoto();
+
+    $datos= $datos."<tr>".
+                        "<td>". $empleado->ToString() ."</td>".
+                        "<td>".
+                            "<img src='$path' height='90px' width='90px'>".
+                        "</td>".
+                    "</tr>";	
+}
+
+$tabla='<table align="">
+            <tr>
+                <td><h4>Info</h4></td>
+            </tr>
+            <tr>
+                <td colspan="4"><hr></td>
+            </tr>'.
+            $datos.
+            '<tr>
+			    <td colspan="4"><hr></td>
+		    </tr>
+	    </table>';
+
+$mpdf->WriteHTML('<h2>Listado de Empleados</h2>');
+
+$mpdf->WriteHTML($tabla);
+
+$mpdf->Output('mi_pdf.pdf', 'I');
+?>
